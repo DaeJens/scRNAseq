25d1cad54178a32e132bef70ca3d7ece79bedbdb
Jensen416 <jensen.416@osu.edu>
2024-02-29 22:21:59 -0500

    First commit: clustering

--- clustering.R
+++ clustering.R
@@ -13,6 +13,7 @@ if (length(missing_packages) > 0) {
     message("All required packages are already installed.")
 }
 
+# Library setup
 library(Seurat)
 library(SeuratObject)
 library(ggplot2)
@@ -26,12 +27,22 @@ path2files <-
 files <- list.files(path = )
 Dataset <- c()
 
+# Take metadata file from command line
+metadata <- 
+meta_df <- 
+
 # Generate Seurat object for each individual sample dataset
 for (x in files) {
     cts <- Read10X_h5(filename = paste0(path2files, x))
-    x <- str_sub(x, start = 1, end = 3)
-    Dataset <- append(Dataset, x)
+    file_base <- tools::file_path_sans_ext(x)
+    sample_name <- sub("_sample_filtered_feature_bc_matrix$", "", file_base)
+    Dataset <- append(Dataset, sample_name)
 
     #create Seurat object
-    assign(x, CreateSeuratObject(counts = cts, min.cells = 3, min.features = 200))
+    assign(sample_name, CreateSeuratObject(counts = cts, min.cells = 3, min.features = 200))
 }
+
+# Merge Seurat objects together
+merged_seurat <- merge()
+
+# Add metadata to the merged Seurat object
